ad4ec92bff409a470b10a8c25c60af89b491340a
IamJoanna <njoroge.joan@strathmore.edu>
2020-12-13 13:25:10 +0300

    Landing and Login page errenous gradle

---
+++ app/src/main/java/com/example/instrumentstore/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.instrumentstore;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button joinNowButton, loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        loginButton = (Button) findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(createPackageContext(MainActivity.this,LoginActivity.class));
+                startActivity(intent);
+
+            }
+        });
+    }
+}
